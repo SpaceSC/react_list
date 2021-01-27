d991af4c73a6983cbc90ade941c73489561dd859
SpaceSC <ceciliasandor.22@gmail.com>
2021-01-27 10:15:02 +0100

    Create react list with checkbox

---
+++ list/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import List from "./components/List.component.jsx";
+
+
+function App() {
+  return (
+    <form className="App">
+      <List />
+    </form>
+  );
+}
+
+export default App;
---
+++ list/src/components/List.component.jsx
@@ -0,0 +1,18 @@
+import "./list.component.css";
+
+function List() {
+  const something = ["position", "display", "border", "padding", "margin", "overflow", "background", "font", "text", "transform"];
+  const listItems = something.map((something) =>
+<li>                              
+  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+  <label for="vehicle1">{something}</label>
+</li>
+);
+
+  return (
+    <form>                            
+     <ul>{listItems}</ul>
+    </form>
+  );
+}
+export default List;
