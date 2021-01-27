43975c86898e6acbc9181e32a686447e7db0d03c
SpaceSC <ceciliasandor.22@gmail.com>
2021-01-27 11:03:04 +0100

    Add unique keys

--- list/src/components/List.component.jsx
+++ list/src/components/List.component.jsx
@@ -1,13 +1,15 @@
 import "./list.component.css";
 
 function List() {
-  const something = ["position", "display", "border", "padding", "margin", "overflow", "background", "font", "text", "transform"];
-  const listItems = something.map((something) =>
-  <li>                              
-    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
-    <label for="vehicle1">{something}</label>
+  const somethings = ["position", "display", "border", "padding", "margin", "overflow", "background", "font", "text", "transform"];
+  const listItems = somethings.map((something) =>
+  <li key={something.toString()}>                              
+    <input type="checkbox" id="attributes" name="attributes"/>
+    <label htmlFor="attributes">{something}</label>
   </li>
 );
+
+// key could also be <li key={something.id}>
 
   return (                   
      <ul>{listItems}</ul>
